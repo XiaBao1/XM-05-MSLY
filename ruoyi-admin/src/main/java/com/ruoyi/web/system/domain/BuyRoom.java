4b396518bcda67fb389fd46a66017b55ff118489
PCH-gg <2892036573@qq.com>
2022-12-08 20:00:50 +0800

    增加了房源管理

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/system/domain/BuyRoom.java
@@ -0,0 +1,107 @@
+package com.ruoyi.web.system.domain;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.ruoyi.common.annotation.Excel;
+import com.ruoyi.common.core.domain.BaseEntity;
+
+/**
+ * 房间订购对象 house_room
+ * 
+ * @author ruoyi
+ * @date 2022-12-02
+ */
+public class BuyRoom extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /**  */
+    private Long id;
+
+    /** 民宿ID */
+    @Excel(name = "民宿ID")
+    private Long houseId;
+
+    /** 房间号 */
+    @Excel(name = "房间号")
+    private String roomNumber;
+
+    /** 每日价格 */
+    @Excel(name = "每日价格")
+    private Long pricePerDay;
+
+    /** 房间标准 */
+    @Excel(name = "房间标准")
+    private String standard;
+
+    /** 空闲 */
+    @Excel(name = "空闲")
+    private Integer isFree;
+
+    public void setId(Long id) 
+    {
+        this.id = id;
+    }
+
+    public Long getId() 
+    {
+        return id;
+    }
+    public void setHouseId(Long houseId) 
+    {
+        this.houseId = houseId;
+    }
+
+    public Long getHouseId() 
+    {
+        return houseId;
+    }
+    public void setRoomNumber(String roomNumber) 
+    {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomNumber() 
+    {
+        return roomNumber;
+    }
+    public void setPricePerDay(Long pricePerDay) 
+    {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public Long getPricePerDay() 
+    {
+        return pricePerDay;
+    }
+    public void setStandard(String standard) 
+    {
+        this.standard = standard;
+    }
+
+    public String getStandard() 
+    {
+        return standard;
+    }
+    public void setIsFree(Integer isFree) 
+    {
+        this.isFree = isFree;
+    }
+
+    public Integer getIsFree() 
+    {
+        return isFree;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("id", getId())
+            .append("houseId", getHouseId())
+            .append("roomNumber", getRoomNumber())
+            .append("pricePerDay", getPricePerDay())
+            .append("standard", getStandard())
+            .append("isFree", getIsFree())
+            .toString();
+    }
+}
