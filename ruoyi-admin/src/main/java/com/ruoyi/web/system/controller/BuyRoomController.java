687341707b23b86689690184a43446d9aeaa2638
PCH-gg <2892036573@qq.com>
2022-12-08 11:11:17 +0800

    备份

--- ruoyi-admin/src/main/java/com/ruoyi/web/system/controller/BuyRoomController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/system/controller/BuyRoomController.java
@@ -1,6 +1,9 @@
 package com.ruoyi.system.controller;
 
+import java.sql.*;
 import java.util.List;
+
+import com.ruoyi.common.json.JSONObject;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,6 +21,9 @@ import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.common.core.page.TableDataInfo;
+import springfox.documentation.spring.web.json.Json;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 房间订购Controller
@@ -127,10 +133,85 @@ public class BuyRoomController extends BaseController
 
 
 
-    @GetMapping("/ff")
+    @PostMapping("/ff")
     @ResponseBody
-    public void ff()
-    {
+    public String ff(HttpServletRequest request) throws SQLException {
+        String sj="";
+        sj="hhhhhhhhhhhhhhhh";
         System.out.println("fff");
+        String houseId=request.getParameter("houseId");
+        String roomNumber=request.getParameter("roomNumber");
+        String price=request.getParameter("price");
+        System.out.println(houseId+"%%%%");
+        System.out.println(roomNumber+"%%%");
+        System.out.println(price+"%%%%%%%%");
+        houseId="4";
+        roomNumber="204";
+        price="100";
+        String roomId="";
+
+        JSONObject res=new JSONObject();
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            //Class.forName("dbDrive");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            res.put("result_code", 1);
+            return res.toString();
+        }
+        System.out.println("JDBC done");
+
+       // try {
+            String url="jdbc:mysql://www.ylxteach.net:3366/xm05_2022?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8";
+            String username="Administrator";
+            String password="XWClassroom20202023";
+           Connection conn=DriverManager.getConnection(url,username,password);
+           conn.close();
+            //Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbproject?user=DataUser&password=301217&useUnicode=true&characterEncoding=UTF-8");
+            //Connection conn = DriverManager.getConnection(" jdbc:mysql://www.ylxteach.net:3366/xm05_2022?user=Administrator&password=XWClassroom20202023&useUnicode=true&characterEncoding=utf8");
+//            System.out.println("statement complete");
+//            Statement statement = conn.createStatement();
+//            System.out.println("sql connected");
+//            String sql = "select * from house_room where house_id = "+houseId+" and room_number = " + roomNumber;
+//            System.out.println("command:" + sql);
+//            ResultSet rs=statement.executeQuery(sql);
+//            System.out.println("66666666666");
+//
+//            while(rs.next()){
+//                int s=Integer.parseInt(rs.getString("is_free"));
+//                roomId=rs.getString("id");
+//                System.out.println("s:"+s);
+//                if(s==0){
+//                    res.put("result_code",1);
+//                    res.put("result_msg","yghdknfbm");
+//                    System.out.println("return here");
+//                    String ans=res.toString();
+//                    System.out.println(ans);
+//                    //return "jjjjjjj";
+//                }
+//            }
+//
+//            sql="insert into client_room_record (client_number,room_id,price,is_paid,is_done) values (5,"+roomId+","+price+",1,0)";
+//            statement.executeUpdate(sql);
+//            System.out.println("command:" + sql);
+//
+//            sql="update house_room set is_free=0 where id = "+roomId;
+//            statement.executeUpdate(sql);
+//            System.out.println("command:" + sql);
+//
+//            conn.close();
+
+
+        //}
+//        catch (SQLException e) {
+//            e.printStackTrace();
+//            res.put("result_code", 1);
+//            return res.toString();
+//        }
+        res.put("result_code", 0);
+        res.put("result_msg","预订成功");
+        System.out.println("wzzlzlzlz");
+        return res.toString();
     }
 }
