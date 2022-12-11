e64a4eb7113e18790bf516c52d0e991e288745a3
ldefault111 <2821806112@qq.com>
2022-12-11 20:12:50 +0800

    增删改查，可排序，加了数据的单位

---
+++ ruoyi-admin/src/main/java/com/ruoyi/weather/controller/WeatherController.java
@@ -0,0 +1,127 @@
+package com.ruoyi.weather.controller;
+
+import java.util.List;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.weather.domain.Weather;
+import com.ruoyi.weather.service.IWeatherService;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.common.core.page.TableDataInfo;
+
+/**
+ * 天气信息Controller
+ * 
+ * @author lrc
+ * @date 2022-12-11
+ */
+@Controller
+@RequestMapping("/weather/weather")
+public class WeatherController extends BaseController
+{
+    private String prefix = "weather/weather";
+
+    @Autowired
+    private IWeatherService weatherService;
+
+    @RequiresPermissions("weather:weather:view")
+    @GetMapping()
+    public String weather()
+    {
+        return prefix + "/weather";
+    }
+
+    /**
+     * 查询天气信息列表
+     */
+    @RequiresPermissions("weather:weather:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(Weather weather)
+    {
+        startPage();
+        List<Weather> list = weatherService.selectWeatherList(weather);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出天气信息列表
+     */
+    @RequiresPermissions("weather:weather:export")
+    @Log(title = "天气信息", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(Weather weather)
+    {
+        List<Weather> list = weatherService.selectWeatherList(weather);
+        ExcelUtil<Weather> util = new ExcelUtil<Weather>(Weather.class);
+        return util.exportExcel(list, "天气信息数据");
+    }
+
+    /**
+     * 新增天气信息
+     */
+    @GetMapping("/add")
+    public String add()
+    {
+        return prefix + "/add";
+    }
+
+    /**
+     * 新增保存天气信息
+     */
+    @RequiresPermissions("weather:weather:add")
+    @Log(title = "天气信息", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(Weather weather)
+    {
+        return toAjax(weatherService.insertWeather(weather));
+    }
+
+    /**
+     * 修改天气信息
+     */
+    @RequiresPermissions("weather:weather:edit")
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, ModelMap mmap)
+    {
+        Weather weather = weatherService.selectWeatherById(id);
+        mmap.put("weather", weather);
+        return prefix + "/edit";
+    }
+
+    /**
+     * 修改保存天气信息
+     */
+    @RequiresPermissions("weather:weather:edit")
+    @Log(title = "天气信息", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(Weather weather)
+    {
+        return toAjax(weatherService.updateWeather(weather));
+    }
+
+    /**
+     * 删除天气信息
+     */
+    @RequiresPermissions("weather:weather:remove")
+    @Log(title = "天气信息", businessType = BusinessType.DELETE)
+    @PostMapping( "/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids)
+    {
+        return toAjax(weatherService.deleteWeatherByIds(ids));
+    }
+}
---
+++ ruoyi-admin/src/main/java/com/ruoyi/weather/domain/Weather.java
@@ -0,0 +1,196 @@
+package com.ruoyi.weather.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.ruoyi.common.annotation.Excel;
+import com.ruoyi.common.core.domain.BaseEntity;
+
+/**
+ * 天气信息对象 weather
+ * 
+ * @author lrc
+ * @date 2022-12-11
+ */
+public class Weather extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 数据编号 */
+    private Long id;
+
+    /** 城市 */
+    @Excel(name = "城市")
+    private String city;
+
+    /** 天气日期 */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Excel(name = "天气日期", width = 30, dateFormat = "yyyy-MM-dd")
+    private Date weatherDate;
+
+    /** 低温 */
+    @Excel(name = "低温")
+    private Long lowT;
+
+    /** 高温 */
+    @Excel(name = "高温")
+    private Long highT;
+
+    /** 白天天气 */
+    @Excel(name = "白天天气")
+    private String dayType;
+
+    /** 夜晚天气 */
+    @Excel(name = "夜晚天气")
+    private String nightType;
+
+    /** 风向 */
+    @Excel(name = "风向")
+    private String windDirection;
+
+    /** 风速（单位m/s） */
+    @Excel(name = "风速", readConverterExp = "单=位m/s")
+    private BigDecimal windSpeed;
+
+    /** 风力（单位级） */
+    @Excel(name = "风力", readConverterExp = "单=位级")
+    private Long windScale;
+
+    /** 湿度（单位%） */
+    @Excel(name = "湿度", readConverterExp = "单=位%")
+    private Long humidity;
+
+    /** 数据收集时间 */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Excel(name = "数据收集时间", width = 30, dateFormat = "yyyy-MM-dd")
+    private Date collectTime;
+
+    public void setId(Long id) 
+    {
+        this.id = id;
+    }
+
+    public Long getId() 
+    {
+        return id;
+    }
+    public void setCity(String city) 
+    {
+        this.city = city;
+    }
+
+    public String getCity() 
+    {
+        return city;
+    }
+    public void setWeatherDate(Date weatherDate) 
+    {
+        this.weatherDate = weatherDate;
+    }
+
+    public Date getWeatherDate() 
+    {
+        return weatherDate;
+    }
+    public void setLowT(Long lowT) 
+    {
+        this.lowT = lowT;
+    }
+
+    public Long getLowT() 
+    {
+        return lowT;
+    }
+    public void setHighT(Long highT) 
+    {
+        this.highT = highT;
+    }
+
+    public Long getHighT() 
+    {
+        return highT;
+    }
+    public void setDayType(String dayType) 
+    {
+        this.dayType = dayType;
+    }
+
+    public String getDayType() 
+    {
+        return dayType;
+    }
+    public void setNightType(String nightType) 
+    {
+        this.nightType = nightType;
+    }
+
+    public String getNightType() 
+    {
+        return nightType;
+    }
+    public void setWindDirection(String windDirection) 
+    {
+        this.windDirection = windDirection;
+    }
+
+    public String getWindDirection() 
+    {
+        return windDirection;
+    }
+    public void setWindSpeed(BigDecimal windSpeed) 
+    {
+        this.windSpeed = windSpeed;
+    }
+
+    public BigDecimal getWindSpeed() 
+    {
+        return windSpeed;
+    }
+    public void setWindScale(Long windScale) 
+    {
+        this.windScale = windScale;
+    }
+
+    public Long getWindScale() 
+    {
+        return windScale;
+    }
+    public void setHumidity(Long humidity) 
+    {
+        this.humidity = humidity;
+    }
+
+    public Long getHumidity() 
+    {
+        return humidity;
+    }
+    public void setCollectTime(Date collectTime) 
+    {
+        this.collectTime = collectTime;
+    }
+
+    public Date getCollectTime() 
+    {
+        return collectTime;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("id", getId())
+            .append("city", getCity())
+            .append("weatherDate", getWeatherDate())
+            .append("lowT", getLowT())
+            .append("highT", getHighT())
+            .append("dayType", getDayType())
+            .append("nightType", getNightType())
+            .append("windDirection", getWindDirection())
+            .append("windSpeed", getWindSpeed())
+            .append("windScale", getWindScale())
+            .append("humidity", getHumidity())
+            .append("collectTime", getCollectTime())
+            .toString();
+    }
+}
