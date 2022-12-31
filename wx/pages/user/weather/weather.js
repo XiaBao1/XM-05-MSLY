ecc69beea09cd035f472d6d7b12303882f08155e
ldefault111 <2821806112@qq.com>
2023-01-01 00:23:50 +0800

    导入天气模板

--- wx/pages/user/weather/weather.js
+++ wx/pages/user/weather/weather.js
@@ -1,66 +1,91 @@
-// pages/user/weather/weather.js
+import util from './../../../utils/util.js';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    sortindex:0,  //排序索引
+    sortid:null,  //排序id
+    sort:[],
+    activitylist:[], //会议室列表列表
+    scrolltop:null, //滚动位置
+    page: 0  //分页
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onLoad: function () { //加载数据渲染页面
+    this.fetchConferenceData();
+    this.fetchSortData();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  fetchSortData:function(){ //获取筛选条件
+    this.setData({
+      "sort": [
+          {
+              "id": 0,
+              "title": "热门点击"
+          },
+          {
+              "id": 1,
+              "title": "最新发布"
+          },
+          {
+              "id": 2,
+              "title": "最多参与"
+          },
+      ]
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  fetchConferenceData:function(){  //获取会议室列表
+    const perpage = 10;
+    this.setData({
+      page:this.data.page+1
+    })
+    const page = this.data.page;
+    const newlist = [];
+    for (var i = (page-1)*perpage; i < page*perpage; i++) {
+      newlist.push({
+        "id":i+1,
+        "name":"云栖技术分享日（云栖TechDay"+(i+1)+"）",
+        "status": "进行中",
+        "time": "2016/07/12 14:00",
+        "coments": Math.floor(Math.random()*1000),
+        "address":"杭州云栖小镇咖啡馆  （杭州云计算产业园内）",
+        "imgurl":"http://pic.58pic.com/58pic/12/34/51/85d58PICkjf.jpg"
+      })
+    }
+    this.setData({
+      activitylist:this.data.activitylist.concat(newlist)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  setSortBy:function(e){ //选择排序方式
+    const d= this.data;
+    const dataset = e.currentTarget.dataset;
+    this.setData({
+      sortindex:dataset.sortindex,
+      sortid:dataset.sortid
+    })
+    console.log('排序方式id：'+this.data.sortid);
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
+  setStatusClass:function(e){ //设置状态颜色
+    console.log(e);
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
+  scrollHandle:function(e){ //滚动事件
+    this.setData({
+      scrolltop:e.detail.scrollTop
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
+  goToTop:function(){ //回到顶部
+    this.setData({
+      scrolltop:0
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  scrollLoading:function(){ //滚动加载
+    this.fetchConferenceData();
+  },
+  onPullDownRefresh:function(){ //下拉刷新
+    this.setData({
+      page:0,
+      activitylist:[]
+    })
+    this.fetchConferenceData();
+    this.fetchSortData();
+    setTimeout(()=>{
+      wx.stopPullDownRefresh()
+    },1000)
   }
 })
