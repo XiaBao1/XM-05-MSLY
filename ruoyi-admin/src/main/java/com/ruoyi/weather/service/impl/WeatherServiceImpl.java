e2eb3a53c3d57db0140dabc7a711e92cec15c55e
ldefault111 <2821806112@qq.com>
2023-01-09 20:40:32 +0800

    完成lrc驾驶舱

--- ruoyi-admin/src/main/java/com/ruoyi/weather/service/impl/WeatherServiceImpl.java
+++ ruoyi-admin/src/main/java/com/ruoyi/weather/service/impl/WeatherServiceImpl.java
@@ -1,9 +1,8 @@
 package com.ruoyi.weather.service.impl;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ruoyi.weather.mapper.WeatherMapper;
@@ -131,6 +130,55 @@ public class WeatherServiceImpl implements IWeatherService
             }
         }
 
+        return ans;
+    }
+
+    /**
+     * 统计本月风向信息
+     *
+     * @return 结果
+     */
+    @Override
+    public List<Long> getMonthDirection(){
+        List<Long> thisMonth=new ArrayList<Long>(31);
+        for(int i=0;i<31;i++){
+            thisMonth.add(null);
+        }
+        Calendar monthStart = Calendar.getInstance();
+        List<Long> ans=new ArrayList<Long>(3);
+        for(int i=0;i<3;i++) {
+            ans.add(0L);
+        }
+        monthStart.set(Calendar.DAY_OF_MONTH,0); // 设成上月最后一天
+        Calendar monthEnd = Calendar.getInstance();
+        monthEnd.set(Calendar.DAY_OF_MONTH, monthEnd.getActualMaximum(Calendar.DAY_OF_MONTH)); //获取当前月最后一天
+        List<Weather> allWeather=weatherMapper.selectWeatherList(new Weather());
+        for(Weather cur:allWeather) {
+            Calendar tmp=Calendar.getInstance();
+            tmp.setTime(cur.getWeatherDate());
+            if(cur.getCity().equals("成都") && monthStart.before(tmp) && tmp.before(monthEnd))
+            {
+                tmp.setTime(cur.getWeatherDate());
+                int id=tmp.get(Calendar.DAY_OF_MONTH);
+                thisMonth.set(id-1,cur.getWindScale());
+            }
+        }
+        for(Long cur:thisMonth) {
+            if(cur==null)
+                continue;
+            if(cur<=2){
+                Long c=ans.get(0);
+                ans.set(0,c+1);
+            }
+            else if(cur<=5){
+                Long c=ans.get(1);
+                ans.set(1,c+1);
+            }
+            else if(cur>5){
+                Long c=ans.get(2);
+                ans.set(2,c+1);
+            }
+        }
         return ans;
     }
 }
